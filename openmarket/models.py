ef8dda7758fa369adda6581e5a1892098e9fd056
lidik <lydiakarungi43@gmail.com>
2020-09-08 17:37:11 +0300

    attributes

--- openmarket/models.py
+++ openmarket/models.py
@@ -40,7 +40,44 @@ class Seller(models.Model):
         return self.seller_type
 
 class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, rela
+     available = models.BooleanField(default=True)
+     date_created = models.DateTimeField(auto_now_add=True)
+     date_updated = models.DateTimeField(auto_now=True)
+
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='seller')
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    business_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    business_location = models.CharField(null=True, max_length=50)
+    seller_type = models.CharField(max_length=15, null=False)
+    date_of_birth = models.DateField(max_length=8)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    county = models.ForeignKey(County, on_delete=models.CASCADE)
+    sub_county = models.ForeignKey(SubCounty, on_delete=models.CASCADE)
+    parish = models.ForeignKey(Parish, on_delete=models.CASCADE)
+    village = models.ForeignKey(Village, on_delete=models.CASCADE)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=15)
+    marital_status = models.CharField(choices=MARITAL_STATUSES, max_length=15, null=False, blank=False)
+    enterprise = models.TextField(null= True)
+    major_products = models.ManyToManyField(Product, related_name='seller')
+
+    class Meta:
+        ordering = ('seller_type',)
+    def __str__(self):
+        return self.seller_type
+
+class Buyer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='buyer')
+
+    class meta:
+        ordering =("name",)
+
+class SellerPost(models.Model):
+    name = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.FloatField(max_length=50, null=True)ted_name='buyer')
 
     class meta:
         ordering =("name",)
