04bc2185d6ee124d0783890cb59934f6bd139a0a
lidik <lydiakarungi43@gmail.com>
2020-09-09 16:35:48 +0300

    farmer serializers

--- openmarket/urls.py
+++ openmarket/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-from .views import ProductList, SellerList, BuyerList
+from .views import ProductList, SellerList, BuyerList, ServiceProviderList, ServiceRegistrationList
 
 
 router = routers.DefaultRouter()
 router.register(r'openmarket', views.ProductViewSet)
 router.register(r'openmarket', views.SellerViewSet)
 router.register(r'openmarket', views.BuyerViewSet)
-
+router.register(r'openmarket', views.ServiceProviderViewSet)
+router.register(r'openmarket', views.ServiceRegistrationViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 
@@ -17,5 +18,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('products', ProductList.as_view(), name='product_list'),
     path('sellers', SellerList.as_view(), name='seller_list'),
-    path('sellers', BuyerList.as_view(), name='buyer_list'),
+    path('buyers', BuyerList.as_view(), name='buyer_list'),
+    path('serviceproviders', ServiceProviderList.as_view(), name='serviceprovider_list'),
+    path('serviceregistration', ServiceRegistrationList.as_view(), name='serviceregistration_list'),
 ]
--- openmarket/views.py
+++ openmarket/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Seller, Buyer, SellerPost, BuyerPost, ServiceProvider, ServiceRegistration, ContactDetails, Logistics, Storage, Packaging, Medical, SoilScience
-from .serializers import ProductSerializer, SellerSerializer, BuyerSerializer, SellerPostSerializer, BuyerPostSerializer
+from .serializers import ProductSerializer, SellerSerializer, BuyerSerializer, SellerPostSerializer, BuyerPostSerializer, ServiceProviderSerializer, ServiceRegistrationSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.renderers import TemplateHTMLRenderer
@@ -77,7 +77,7 @@ class SellerPostList(APIView):
     template_name = 'sellerpost_list.html'
 
     def get(self, request):
-        queryset = sellerpost.objects.order_by('-name')
+        queryset = SellerPost.objects.order_by('-name')
         return Response({'sellerposts': queryset})
 
 
@@ -98,3 +98,41 @@ class BuyerPostList(APIView):
     def get(self, request):
         queryset = BuyerPost.objects.order_by('name')
         return Response({'buyerposts': queryset})
+
+
+#views for service provider
+class ServiceProviderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows products to be viewed or edited.
+    """
+    queryset = ServiceProvider.objects.all().order_by('name')
+    serializer_class = ServiceProviderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ServiceProviderList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'serviceprovider_list.html'
+
+    def get(self, request):
+        queryset = ServiceProvider.objects.order_by('-name')
+        return Response({'serviceproviders': queryset})
+
+
+#views for service registration
+class ServiceRegistrationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows products to be viewed or edited.
+    """
+    queryset = ServiceRegistration.objects.all().order_by('name')
+    serializer_class = ServiceRegistrationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ServiceRegistrationList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'serviceregistration_list.html'
+
+    def get(self, request):
+        queryset = ServiceRegistration.objects.order_by('-name')
+        return Response({'serviceregistration': queryset})
