99960324be28155682088c86f92c63a018fe0ff8
angelnagaba <angelnagaba99@gmail.com>
2020-09-09 09:40:50 -0400

    made changes to urls file

--- farmer/urls.py
+++ farmer/urls.py
@@ -1,15 +1,10 @@
-from farmer.views import [
-    FarmerView,
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+from .views import FarmerView
 
-]
-
-
-app_name = 'farmer'
+app_name = 'farmers'
 
 urlpatterns = [
-     path('', FarmerView.as_view(), name='farmer'),
-    # path('', FarmerView.as_view(), name='farmer'),
-
-
 
 ]
