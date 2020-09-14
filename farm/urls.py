334daaf2383f977233380351d886d9b8426871d5
musarahim13@gmail.com <musarahim13@gmail.com>
2020-09-14 16:57:54 +0300

    resolved conflicts from master

--- farm/urls.py
+++ farm/urls.py
@@ -7,13 +7,11 @@ from .views import SectorList, SectorDetail, CreateSector,EnterpriseList
 
 
 router = routers.DefaultRouter()
-<<<<<<< HEAD
 router.register(r'farms', views.SectorViewSet)
 router.register(r'farms', views.EnterpriseViewSet)
-=======
+
 router.register(r'sector', views.SectorViewSet)
 router.register(r'enterprise', views.EnterpriseViewSet)
->>>>>>> 80d82ddd3d57a564901efc611a3b0d8a79fd6dc8
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
