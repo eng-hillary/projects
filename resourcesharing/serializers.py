ca613fc3e1c865d2fb15fb0cf1fd678b0e06966c
lidik-ops <lydiakarungi43@gmail.com>
2020-10-15 17:16:28 +0300

    changes

--- resourcesharing/serializers.py
+++ resourcesharing/serializers.py
@@ -4,14 +4,17 @@ from .models import (Resource, ResourceSharing, ResourceBooking)
 
 class ResourceSerializer(serializers.ModelSerializer):
     owner = serializers.SerializerMethodField(method_name='get_user_full_name',source='user')
+    district = serializers.SerializerMethodField(method_name='get_district',source='owner')
     class Meta:
         model = Resource
-        fields = ['id','resource_name', 'owner', 'Phone_number1', 'Phone_number2','resource_category', 'lat', 'lon',
+        fields = ['id','resource_name','district', 'owner', 'Phone_number1', 'Phone_number2','resource_category', 'lat', 'lon',
         'terms_and_conditions', 'resource_status', 'available_from','available_to', 'price','image']
 
     def get_user_full_name(self, obj):
         return '{} {}'.format(obj.owner.user.first_name, obj.owner.user.last_name)
 
+    def get_district(self, obj):
+        return '{}'.format(obj.owner.district.name)
 
 class ResourceSharingSerializer(serializers.ModelSerializer):
     class Meta:
