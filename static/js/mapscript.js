5a53ca3e0e9384f447f590369c0b595b63e56805
lidik-ops <lydiakarungi43@gmail.com>
2020-10-26 11:07:12 +0300

    added filters on pie chart

--- static/js/mapscript.js
+++ static/js/mapscript.js
@@ -403,228 +403,87 @@ Highcharts.chart('piecontainer', {
     series: {
       dataLabels: {
         enabled: true,
-        format: '{point.name}: {point.y:.1f}%'
+        format: '{point.name}: {point.y:.1f}%<br>' 
       }
     }
   },
 
   tooltip: {
     headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total farmers<br/>'
+    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total farmers<br/>'+
+                 '<span style="color:{point.color}">{point.sectorone}</span>: <b>{point.one:.2f}%</b> of total sectors<br/>'+
+                 '<span style="color:{point.color}">{point.sectortwo}</span>: <b>{point.two:.2f}%</b> of total sectors<br/>'+
+                 '<span style="color:{point.color}">{point.sectorthree}</span>: <b>{point.three:.2f}%</b> of total sectors<br/>'+
+                 '<span style="color:{point.color}">{point.sectorfour}</span>: <b>{point.four:.2f}%</b> of total sectors<br/>'
   },
 
   series: [
     {
-      name: "Browsers",
+      name: "Farmers",
       colorByPoint: true,
       data: [
         {
           name: "Western",
+          sectorone: "livestock",
+          sectortwo: "poultry",
+          sectorthree: "fishery",
+          sectorfour: "crops",
           y: 62.74,
-          drilldown: "Western"
+          one: 15.68,
+          two: 3.92,
+          three: 3.92,
+          four: 3.92
         },
         {
           name: "Northern",
+          sectorone: "Livestock",
+          sectortwo: "Poultry",
+          sectorthree: "Pishery",
+          sectorfour: "Crops",
           y: 10.57,
-          drilldown: "Northern"
+          one: 15.68,
+          two: 3.92,
+          three: 3.92,
+          four: 3.92
         },
         {
           name: "Central",
+          sectorone: "livestock",
+          sectortwo: "poultry",
+          sector3: "fishery",
+          sector4: "crops",
           y: 7.23,
-          drilldown: "Central"
+          one: 15.68,
+          two: 3.92,
+          three: 3.92,
+          four: 3.92
         },
         {
           name: "Eastern",
+          sectorone: "livestock",
+          sectortwo: "poultry",
+          sector3: "fishery",
+          sector4: "crops",
           y: 5.58,
-          drilldown: "Eastern"
+          one: 15.68,
+          two: 3.92,
+          three: 3.92,
+          four: 3.92
         }
       ]
     }
-  ],
-  drilldown: {
-    series: [
-      {
-        name: "Eastern Region",
-        id: "Eastern Region",
-        data: [
-          [
-            "v65.0",
-            0.1
-          ],
-          [
-            "v64.0",
-            1.3
-          ],
-          [
-            "v63.0",
-            53.02
-          ],
-          [
-            "v62.0",
-            1.4
-          ],
-          [
-            "v61.0",
-            0.88
-          ],
-          [
-            "v60.0",
-            0.56
-          ],
-          [
-            "v59.0",
-            0.45
-          ],
-          [
-            "v58.0",
-            0.49
-          ],
-          [
-            "v57.0",
-            0.32
-          ],
-          [
-            "v56.0",
-            0.29
-          ],
-          [
-            "v55.0",
-            0.79
-          ],
-          [
-            "v54.0",
-            0.18
-          ],
-          [
-            "v51.0",
-            0.13
-          ],
-          [
-            "v49.0",
-            2.16
-          ],
-          [
-            "v48.0",
-            0.13
-          ],
-          [
-            "v47.0",
-            0.11
-          ],
-          [
-            "v43.0",
-            0.17
-          ],
-          [
-            "v29.0",
-            0.26
-          ]
-        ]
-      },
-      {
-        name: "Western Region",
-        id: "Western Region",
-        data: [
-          [
-            "v58.0",
-            1.02
-          ],
-          [
-            "v57.0",
-            7.36
-          ],
-          [
-            "v56.0",
-            0.35
-          ],
-          [
-            "v55.0",
-            0.11
-          ],
-          [
-            "v54.0",
-            0.1
-          ],
-          [
-            "v52.0",
-            0.95
-          ],
-          [
-            "v51.0",
-            0.15
-          ],
-          [
-            "v50.0",
-            0.1
-          ],
-          [
-            "v48.0",
-            0.31
-          ],
-          [
-            "v47.0",
-            0.12
-          ]
-        ]
-      },
-      {
-        name: "Northen Region",
-        id: "Northen Region",
-        data: [
-          [
-            "v11.0",
-            6.2
-          ],
-          [
-            "v10.0",
-            0.29
-          ],
-          [
-            "v9.0",
-            0.27
-          ],
-          [
-            "v8.0",
-            0.47
-          ]
-        ]
-      },
-      {
-        name: "Central Region",
-        id: "Central Region",
-        data: [
-          [
-            "v11.0",
-            6.2
-          ],
-          [
-            "v10.0",
-            0.29
-          ],
-          [
-            "v9.0",
-            0.27
-          ],
-          [
-            "v8.0",
-            0.47
-          ]
-        ]
-      }
-    ]
-  }
+  ]
 });
  
 
 // Create the chart
-
 Highcharts.getJSON('/farmer/api/farmerprofiles/', function (json) {
   var data = [];
   json.forEach(function (p) {
       p.z = p.region;
       data.push(p);
   })
-console.log(data);
+//console.log(data);
 $(function() {
     var region, name;
     Highcharts.chart('barcontainer', {
