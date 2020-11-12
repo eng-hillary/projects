d35cdfbda805d180db78c6dbc35b1387186dd57e
musarahim13@gmail.com <musarahim13@gmail.com>
2020-11-12 15:33:01 +0300

    changed initial point field for Oswidget

--- weather/forms.py
+++ weather/forms.py
@@ -4,9 +4,10 @@ from django.contrib.gis import forms
 from django.contrib.gis.geos import Point
 
 
+
 class WeatherForm(forms.ModelForm):
     location = forms.PointField(widget=forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500, 'mouse_position': True,'default_zoom':7}),
-    initial=Point(x=1.0609637, y=32.5672804, srid=4326,z=6.98))
+    initial=Point(y=1.0609637, x=32.5672804, srid=4326,z=6.98))
 
   
     class Meta:
