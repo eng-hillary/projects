4a7cf403009a3be69c1737938d7583b1aebc2db6
musarahim13@gmail.com <musarahim13@gmail.com>
2020-11-24 17:15:27 +0300

    changed hover css

--- weather/forms.py
+++ weather/forms.py
@@ -7,7 +7,7 @@ from django.contrib.gis.geos import Point
 
 class WeatherForm(forms.ModelForm):
     location = forms.PointField(widget=forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500, 'mouse_position': True,'default_zoom':7}),
-    initial=Point(y=1.0609637, x=32.5672804, srid=4326,z=6.98))
+    initial=Point(y=1.0609637, x=32.5672804, srid=4326))
 
   
     class Meta:
