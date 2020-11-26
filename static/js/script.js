f225d3db66955e874e32b84866cdb7411c1e0ae8
lidik-ops <lydiakarungi43@gmail.com>
2020-11-26 16:10:35 +0300

    maps

--- static/js/script.js
+++ static/js/script.js
@@ -147,8 +147,8 @@ $(document).ready(function () {
       var community_weather_url = "/weather/api/community_weather/?lon="+crd.longitude+"&lat="+crd.latitude;
       console.log(community_weather_url);
       // console.log('Your current position is:');
-      console.log(`Latitude : ${crd.latitude}`);
-      console.log(`Longitude: ${crd.longitude}`);
+     // console.log(`Latitude : ${crd.latitude}`);
+     // console.log(`Longitude: ${crd.longitude}`);
       // console.log(`More or less ${crd.accuracy} meters.`);
       $.ajax({
         url: wheather_api,
@@ -175,7 +175,7 @@ $(document).ready(function () {
         type:'GET',
         data:{},
         success:function(data){
-          console.log(data);
+         // console.log(data);
           var community_weather='';
           $.each(data, function(index, val){
             var village =val.village.split(",")[0];
@@ -342,7 +342,7 @@ $(document).ready(function () {
          // console.log(objectFromOWM);
         },
         error: function(response){
-          console.log(response);
+         // console.log(response);
         }
       });
     
@@ -370,3 +370,95 @@ $(document).ready(function () {
   });
 
   
+  var map;
+  var icon = "images/farms.png";
+  var json = "/farm/api/maps/";
+  console.log("-------");
+  console.log(json);
+
+  function initialize() {
+    var infowindow = new google.maps.InfoWindow();
+    var mapProp = {
+      center: new google.maps.LatLng(1.0609637, 32.5672804), //LLANDRINDOD WELLS
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+  
+    map = new google.maps.Map(document.getElementById("map"), mapProp);
+  
+    //  $.getJSON(json, function(json1) {
+    var json1 = {
+      "farms": [{
+            "id": 1,
+            "region": "western",
+            "phone_number": "+256782998057",
+            "district": "kabarole",
+            "farm_name": "poultry farm",
+            "farmer": "karungi lydia",
+            "lat": 1.2710068,
+            "lon": 31.1084552,
+            "land_occupied": 9.0
+        }
+    ]
+    };
+    $.each(json1.farms, function(key, data) {
+  
+      var latlon = new google.maps.LatLng(data.lat, data.lon);
+  
+      var marker = new google.maps.Marker({
+        position: latlon,
+        map: map,
+        // icon: icon,
+        farm_name: data.farm_name
+      });
+  
+      
+      var details = "ID:" + data.id + "<br>" + "REGION:" + data.region + "<br>" + "DISTRICT:" +
+      data.district + "<br>" + "FARMER:" + data.farmer + "<br>" + "PHONE NUMBER:" + data.phone_number + ".";
+  
+      bindInfoWindow(marker, map, infowindow, details);
+  
+      //    });
+  
+    });
+  
+  }
+  
+  function bindInfoWindow(marker, map, infowindow, strDescription) {
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(strDescription);
+      infowindow.open(map, marker);
+    });
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+// var map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 8,
+//     center: new google.maps.LatLng(1.0609637, 32.5672804),
+//     mapTypeId: "terrain",
+//   });
+//   // Create a <script> tag and set the USGS URL as the source.
+//   const script = document.createElement("script");
+//   // This example uses a local copy of the GeoJSON stored at
+//   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+//   script.src =
+//     "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+//   document.getElementsByTagName("head")[0].appendChild(script);
+// }
+
+// // Loop through the results array and place a marker for each
+// // set of coordinates.
+// const eqfeed_callback = function (results) {
+//   for (let i = 0; i < results.features.length; i++) {
+//     const coords = results.features[i].geometry.coordinates;
+//     const latLng = new google.maps.LatLng(coords[1], coords[0]);
+//     new google.maps.Marker({
+//       position: latLng,
+//       map: map,
+//     });
+//   }
+// };
